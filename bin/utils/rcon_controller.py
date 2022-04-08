49b4b8296a420f3e1dee63b1ed67a16fef52b070
TheGreatStorm <thegreatstormx@gmail.com>
2022-04-08 14:15:34 -0500

    Fixing localhost to 0.0.0.0 Bug causing it to crash on run.

--- bin/utils/rcon_controller.py
+++ bin/utils/rcon_controller.py
@@ -2,14 +2,14 @@ from mcrcon import MCRcon
 
 
 def with_connect(rcon_port, rcon_password, rcon_command):
-    with MCRcon("localhost:" + rcon_port, rcon_command) as mcr:
+    with MCRcon("0.0.0.0:" + rcon_port, rcon_command) as mcr:
         #/whitelist add bob
         resp = mcr.command(rcon_command)
         print(resp)
 
 
 def direct_connect(rcon_port, rcon_password, rcon_command):
-    mcr = MCRcon("localhost:" + rcon_port, rcon_command)
+    mcr = MCRcon("0.0.0.0:" + rcon_port, rcon_command)
     # connect to minecraft server
     mcr.connect()
     resp = mcr.command(rcon_command)
